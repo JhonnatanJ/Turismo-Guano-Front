df1d46253921be6ea299f9427902098d962728ca
JhonnatanJ <javiermarquez_99@hotmail.com>
2023-12-04 17:44:21 -0500

    style(producto): modifica interfaz de productos, agrega grid de card producto

--- src/app/shared/components/card-producto/card-producto.component.ts
+++ src/app/shared/components/card-producto/card-producto.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Producto {
+  nombre: string;
+  etiqueta: string;
+  imagen: string;
+  descripcion: string;
+  likes: number;
+  precio: number;
+}
 
 @Component({
-  selector: 'app-card-producto',
+  selector: 'shared-card-producto',
   templateUrl: './card-producto.component.html',
-  styleUrls: ['./card-producto.component.css']
+  styleUrls: ['./card-producto.component.css'],
 })
 export class CardProductoComponent {
 
+  @Input() producto!: Producto;
+
+  // producto: Producto = {
+  //   nombre: 'Funda de leche - 1 litro',
+  //   etiqueta: 'Leche',
+  //   imagen:
+  //   'https://www.supermercadosantamaria.com/documents/10180/10504/79347_G.jpg',
+  //   descripcion: 'Funda de leche de 1 litro con normas de calidad adecuadas',
+  //   likes: 15,
+  //   precio: 1.2,
+  // };
 }
