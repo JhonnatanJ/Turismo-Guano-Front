2457d4ebbbb99eb0e1cacf18c8b3a130c8b8b8b3
JhonnatanJ <javiermarquez_99@hotmail.com>
2023-12-04 01:21:14 -0500

    feat(turismo): muestra grid(8x2) de cards en componente turismo
    card turismo recibe actividad para mostrar datos

--- src/app/shared/components/card-turismo/card-turismo.component.ts
+++ src/app/shared/components/card-turismo/card-turismo.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+
+interface Actividad{
+  nombre: string;
+  etiqueta: string;
+  imagen: string;
+  descripcion: string;
+  likes: number;
+}
 
 @Component({
   selector: 'shared-card-turismo',
@@ -7,15 +16,17 @@ import { Component } from '@angular/core';
 })
 export class CardTurismoComponent {
 
-  actividad = {
-    etiqueta: 'Senderismo',
-    imagen:
-      'https://img.freepik.com/foto-gratis/paisaje-colina-cubierta-vegetacion-excursionistas-subiendola-cielo-nublado_181624-13407.jpg?w=1060&t=st=1701654111~exp=1701654711~hmac=44acbd5200bda637e519cc0c32ef5963efe521eebf820f8e71b86d9ae147fcea',
-    nombre: 'Las Lagunas Mágicas de San Rafael: Ruta Piedra Bolivar',
-    descripcion:
-      'Bienvenido, esta actividad presente en la ruta Piedra Bolivar inicia con la salida en Consectetur velit est nulla est dolore occaecat amet aliqua excepteur irure. Pariatur in id reprehenderit ullamco nulla tempor ex quis et dolore. Duis do nulla duis elit ex magna. Consectetur in pariatur proident irure fugiat adipisicing consequat aliqua anim velit excepteur id...',
-    likes: 20,
-  };
+  @Input() actividad!: Actividad;
+
+  // actividad = {
+  //   etiqueta: 'Senderismo',
+  //   imagen:
+  //     'https://img.freepik.com/foto-gratis/paisaje-colina-cubierta-vegetacion-excursionistas-subiendola-cielo-nublado_181624-13407.jpg?w=1060&t=st=1701654111~exp=1701654711~hmac=44acbd5200bda637e519cc0c32ef5963efe521eebf820f8e71b86d9ae147fcea',
+  //   nombre: 'Las Lagunas Mágicas de San Rafael: Ruta Piedra Bolivar',
+  //   descripcion:
+  //     'Bienvenido, esta actividad presente en la ruta Piedra Bolivar inicia con la salida en Consectetur velit est nulla est dolore occaecat amet aliqua excepteur irure. Pariatur in id reprehenderit ullamco nulla tempor ex quis et dolore. Duis do nulla duis elit ex magna. Consectetur in pariatur proident irure fugiat adipisicing consequat aliqua anim velit excepteur id...',
+  //   likes: 20,
+  // };
 
 
 }
